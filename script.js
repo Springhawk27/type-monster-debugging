8ffac120c2eeb0c4fa8fb44c8c9d6ecbff4615d5
Springhawk27 <sajjad0mahmud@gmail.com>
2022-09-15 15:47:07 +0600

    a few adjustments

--- script.js
+++ script.js
@@ -138,3 +138,10 @@ setInterval(() => {
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
+
+
+window.addEventListener('keydown', function (e) {
+  if (e.keyCode == 32 && e.target == document.body) {
+    e.preventDefault();
+  }
+});
